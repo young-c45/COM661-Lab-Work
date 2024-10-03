aa1eed3787ca36ecc110673667f8e64aab199e70
Callum Young <Young-C45@ulster.ac.uk>
2024-10-03 14:02:16 +0100

    Lab 4.1

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
 from flask import Flask, make_response, jsonify, request
+import uuid, random
 
 app = Flask(__name__)
 
-# Example of stored data
-businesses = [
-    {
-        "id" : 1,
-        "name" : "Pizza Mountain",
-        "town" : "Coleraine",
-        "rating" : 5,
-        "reviews" : []
-    },
-    {
-        "id" : 2,
-        "name" : "Wine Lake",
-        "town" : "Ballymoney",
-        "rating" : 3,
-        "reviews" : []
-    },
-    {
-        "id" : 3,
-        "name" : "Sweet Desert",
-        "town" : "Ballymena",
-        "rating" : 4,
-        "reviews" : []
-    }
-]
+businesses = {}
+
+# For creating an example dataset
+def generate_dummy_data():
+    towns = ["Coleraine", "Banbridge", "Belfast", "Lisburn", "Ballymena",
+        "Derry", "Newry", "Enniskillen", "Omagh", "Ballymoney"]
+    buisness_dict = {}
+    for i in range(100):
+        id = str(uuid.uuid1())
+        name = "Biz " + str(i)
+        town = towns[random.randint(0, len(towns)-1)]
+        rating = random.randint(1,5)
+        buisness_dict[id] = {
+            "name": name,
+            "town": town,
+            "rating": rating,
+            "reviews": {}
+        }
+    return buisness_dict
 
 # Retrieve request for all businesses
 @app.route("/api/v1.0/businesses", methods=["GET"])
@@ -33,112 +29,77 @@ def show_all_businesses():
     return make_response( jsonify( businesses ), 200 )
 
 # Retrieve request for specific business
-@app.route("/api/v1.0/businesses/<int:id>", methods=["GET"])
+@app.route("/api/v1.0/businesses/<string:id>", methods=["GET"])
 def show_one_business(id):
-    data_to_return = [ 
-        business for business in businesses if business["id"] == id ]
-    return make_response(jsonify(data_to_return[0]), 200)
+    return make_response(jsonify(businesses[id]), 200)
 
 # Create business request
 @app.route("/api/v1.0/businesses", methods=["POST"])
 def add_business():
-    next_id = businesses[-1]["id"] + 1
+    next_id = str(uuid.uuid1)
     new_business = {
-        "id": next_id,
         "name": request.form["name"],
         "town": request.form["town"],
         "rating": request.form["rating"],
         "reviews": []
     }
-    businesses.append(new_business)
-    return make_response(jsonify(new_business), 201)
+    businesses[next_id] = new_business
+    return make_response(jsonify({next_id: new_business}), 201)
 
 # Update business request
-@app.route("/api/v1.0/businesses/<int:id>", methods=["PUT"])
+@app.route("/api/v1.0/businesses/<string:id>", methods=["PUT"])
 def edit_business(id):
-    for business in businesses:
-        if business["id"] == id:
-            business["name"] = request.form["name"]
-            business["town"] = request.form["town"]
-            business["rating"] = request.form["rating"]
-            break
-    return make_response(jsonify(business), 200)
+    businesses[id]["name"] = request.form["name"]
+    businesses[id]["town"] = request.form["town"]
+    businesses[id]["rating"] = request.form["rating"]
+    return make_response(jsonify({id: businesses[id]}), 200)
 
 # Delete business request
-@app.route("/api/v1.0/businesses/<int:id>", methods=["DELETE"])
+@app.route("/api/v1.0/businesses/<string:id>", methods=["DELETE"])
 def delete_business(id):
-    for business in businesses:
-        if business["id"] == id:
-            businesses.remove(business)
-            break
+    del businesses[id]
     return make_response(jsonify({}), 200)
 
 # Retrieve request for all reviews
-@app.route("/api/v1.0/businesses/<int:id>/reviews", methods=["GET"])
+@app.route("/api/v1.0/businesses/<string:id>/reviews", methods=["GET"])
 def fetch_all_reviews(id):
-    for business in businesses:
-        if business["id"] == id:
-            break
-    return make_response( jsonify(business["reviews"]), 200)
+    return make_response( jsonify(businesses[id]["reviews"]), 200)
 
 # Retrieve request for specific review
-@app.route("/api/v1.0/businesses/<int:b_id>/reviews/<int:r_id>", 
+@app.route("/api/v1.0/businesses/<string:b_id>/reviews/<string:r_id>", 
     methods=["GET"])
 def fetch_one_review(b_id, r_id):
-    for business in businesses:
-        if business["id"] == b_id:
-            for review in business["reviews"]:
-                if review["id"] == r_id:
-                    break
-            break
-    return make_response(jsonify(review), 200)
+    return make_response(jsonify(businesses[b_id]["reviews"][r_id]), 200)
 
 # Create review request
-@app.route("/api/v1.0/businesses/<int:b_id>/reviews", methods=["POST"])
+@app.route("/api/v1.0/businesses/<string:b_id>/reviews", methods=["POST"])
 def add_new_review(b_id):
-    for business in businesses:
-        if business["id"] == b_id:
-            if len(business["reviews"]) == 0:
-                new_review_id = 1
-            else:
-                new_review_id = business["reviews"][-1]["id"] + 1
-            new_review = {
-                "id": new_review_id,
-                "username": request.form["username"],
-                "comment": request.form["comment"],
-                "stars": request.form["stars"]
-            }
-            business["reviews"].append(new_review)
-            break
-    return make_response(jsonify(new_review), 201)
+    new_review_id = str(uuid.uuid1())
+    new_review = {
+        "username": request.form["username"],
+        "comment": request.form["comment"],
+        "stars": request.form["stars"]
+    }
+    businesses[b_id]["reviews"][new_review_id] = new_review
+    return make_response(jsonify({new_review_id: new_review}), 201)
 
 # Update review request
-@app.route("/api/v1.0/businesses/<int:b_id>/reviews/<int:r_id>", 
+@app.route("/api/v1.0/businesses/<string:b_id>/reviews/<string:r_id>", 
     methods=["PUT"])
 def edit_review(b_id, r_id):
-    for business in businesses:
-        if business["id"] == b_id:
-            for review in business["reviews"]:
-                if review["id"] == r_id:
-                    review["username"] = request.form["username"]
-                    review["comment"] = request.form["comment"]
-                    review["stars"] = request.form["stars"]
-                    break
-            break
-    return make_response(jsonify(review), 200)
+    businesses[b_id]["reviews"][r_id]["username"] = request.form["username"]
+    businesses[b_id]["reviews"][r_id]["comment"] = request.form["comment"]
+    businesses[b_id]["reviews"][r_id]["stars"] = request.form["stars"]
+    return make_response(jsonify( \
+        {r_id: businesses[b_id]["reviews"][r_id]}), 201)
 
 # Delete review request
-@app.route("/api/v1.0/businesses/<int:b_id>/reviews/<int:r_id>", 
+@app.route("/api/v1.0/businesses/<string:b_id>/reviews/<string:r_id>", 
     methods=["DELETE"])
 def delete_review(b_id, r_id):
-    for business in businesses:
-        if business["id"] == b_id:
-            for review in business["reviews"]:
-                if review["id"] == r_id:
-                    business["reviews"].remove(review)
-                    break
-            break
+    del businesses[b_id]["reviews"][r_id]
     return make_response(jsonify({}), 200)
 
 if __name__ == "__main__":
+    businesses = generate_dummy_data()
     app.run(debug=True)
