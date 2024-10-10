0d9faf7363ff289942b1fb1fe19f22e5795e0253
Callum Young <Young-C45@ulster.ac.uk>
2024-10-10 14:13:33 +0100

    Lab 6.2

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 
 client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.bizDB
-buisnesses = db.biz
-
-
-
-"""
+businesses = db.biz
 
 # Retrieve request for all businesses
 @app.route("/api/v1.0/businesses", methods=["GET"])
@@ -22,16 +18,31 @@ def show_all_businesses():
         page_size = int(request.args.get('ps'))
         
     page_start = (page_size * (page_num - 1))
-    businesses_list = [{k : v} for k,v in businesses.items()]
-    data_to_return = businesses_list[page_start : page_start+page_size]
+
+    data_to_return = []
+    for business in businesses.find().skip(page_start).limit(page_size):
+        # Converts ids to strings
+        business['_id'] = str(business['_id'])
+        for review in business['reviews']:
+            review['_id'] = str(review['_id'])
+        data_to_return.append(business)
 
     return make_response( jsonify( data_to_return ), 200 )
 
 # Retrieve request for specific business
 @app.route("/api/v1.0/businesses/<string:id>", methods=["GET"])
 def show_one_business(id):
-    if id in businesses:
-        return make_response(jsonify(businesses[id]), 200)
+    try:
+        business = businesses.find_one({'_id':ObjectId(id)})
+    except:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
+
+    if business is not None:
+        # Converts ids to strings
+        business['_id'] = str(business['_id'])
+        for review in business['reviews']:
+            review['_id'] = str(review['_id'])
+        return make_response(jsonify(business), 200)
     else:
         return make_response(jsonify({"error": "Invalid business ID"}), 404)
 
@@ -43,40 +54,58 @@ def add_business():
     or "rating" not in  request.form:
         return make_response(jsonify({"error": "Missing form data"}), 404)
 
-    next_id = str(uuid.uuid1())
     new_business = {
         "name": request.form["name"],
         "town": request.form["town"],
         "rating": request.form["rating"],
         "reviews": []
     }
-    businesses[next_id] = new_business
-    return make_response(jsonify({next_id: new_business}), 201)
+    new_business_id = businesses.insert_one(new_business)
+    new_business_link = "http://localhost:5000/api/v1.0/businesses/" \
+        + str(new_business_id.inserted_id)
+    return make_response(jsonify({"url": new_business_link}), 201)
 
 # Update business request
 @app.route("/api/v1.0/businesses/<string:id>", methods=["PUT"])
 def edit_business(id):
-    if id not in businesses:
-        return make_response(jsonify({"error": "Invalid business ID"}), 404)
-
-    is_edited = False
-    for key, value in request.form.items():
-        if key in businesses[id]:
-            is_edited = True
-            businesses[id][key] = value
-    if is_edited:
-        return make_response(jsonify({id: businesses[id]}), 200)
-    else:
+    if "name" not in request.form \
+    or "town" not in request.form \
+    or "rating" not in  request.form:
         return make_response(jsonify({"error": "Missing form data"}), 404)
+    
+    try:
+        idObject = ObjectId(id)
+    except:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
+    
+    result = businesses.update_one({"_id": idObject}, {"$set": {
+        "name": request.form["name"],
+        "town": request.form["town"],
+        "rating": request.form["rating"]
+    }})
+
+    if result.matched_count == 1:
+        edited_buisness_link = "http://localhost:5000/api/v1.0/businesses/" \
+        + id
+        return make_response(jsonify({"url": edited_buisness_link}), 200)
+    else:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
 
 # Delete business request
 @app.route("/api/v1.0/businesses/<string:id>", methods=["DELETE"])
 def delete_business(id):
-    if id not in businesses:
+    try:
+        result = businesses.delete_one({"_id": ObjectId(id)})
+    except:
         return make_response(jsonify({"error": "Invalid business ID"}), 404)
     
-    del businesses[id]
-    return make_response(jsonify({}), 200)
+    if result.deleted_count == 1:
+        return make_response(jsonify({}), 200)
+    else:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
+
+
+"""
 
 # Retrieve request for all reviews
 @app.route("/api/v1.0/businesses/<string:id>/reviews", methods=["GET"])
