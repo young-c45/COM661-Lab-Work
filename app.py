9dcdd9a60487bb534cac8e1c48c7051c2ee1af18
Callum Young <Young-C45@ulster.ac.uk>
2024-10-10 15:03:23 +0100

    Lab 6.3

--- app.py
+++ app.py
@@ -8,6 +8,25 @@ client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.bizDB
 businesses = db.biz
 
+# Create business request
+@app.route("/api/v1.0/businesses", methods=["POST"])
+def add_business():
+    if "name" not in request.form \
+    or "town" not in request.form \
+    or "rating" not in  request.form:
+        return make_response(jsonify({"error": "Missing form data"}), 404)
+
+    new_business = {
+        "name": request.form["name"],
+        "town": request.form["town"],
+        "rating": request.form["rating"],
+        "reviews": []
+    }
+    new_business_id = businesses.insert_one(new_business)
+    new_business_link = "http://localhost:5000/api/v1.0/businesses/" \
+        + str(new_business_id.inserted_id)
+    return make_response(jsonify({"url": new_business_link}), 201)
+
 # Retrieve request for all businesses
 @app.route("/api/v1.0/businesses", methods=["GET"])
 def show_all_businesses():
@@ -45,25 +64,6 @@ def show_one_business(id):
         return make_response(jsonify(business), 200)
     else:
         return make_response(jsonify({"error": "Invalid business ID"}), 404)
-
-# Create business request
-@app.route("/api/v1.0/businesses", methods=["POST"])
-def add_business():
-    if "name" not in request.form \
-    or "town" not in request.form \
-    or "rating" not in  request.form:
-        return make_response(jsonify({"error": "Missing form data"}), 404)
-
-    new_business = {
-        "name": request.form["name"],
-        "town": request.form["town"],
-        "rating": request.form["rating"],
-        "reviews": []
-    }
-    new_business_id = businesses.insert_one(new_business)
-    new_business_link = "http://localhost:5000/api/v1.0/businesses/" \
-        + str(new_business_id.inserted_id)
-    return make_response(jsonify({"url": new_business_link}), 201)
 
 # Update business request
 @app.route("/api/v1.0/businesses/<string:id>", methods=["PUT"])
@@ -104,24 +104,51 @@ def delete_business(id):
     else:
         return make_response(jsonify({"error": "Invalid business ID"}), 404)
 
+# Create review request
+@app.route("/api/v1.0/businesses/<string:b_id>/reviews", methods=["POST"])
+def add_new_review(b_id):
+    if "username" not in request.form \
+    or "comment" not in request.form \
+    or "stars" not in  request.form:
+        return make_response(jsonify({"error": "Missing form data"}), 404)
+    
+    try:
+        b_idObject = ObjectId(b_id)
+    except:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
 
-"""
+    new_review = {
+        "_id": ObjectId(),
+        "username": request.form["username"],
+        "comment": request.form["comment"],
+        "stars": request.form["stars"]
+    }
+
+    result = businesses.update_one({"_id": b_idObject}, \
+        {"$push": {"reviews": new_review}})
+    if result.matched_count == 1:
+        new_review_link = "http://localhost:5000/api/v1.0/businesses/" \
+            + b_id + "/reviews/" + str(new_review['_id'])
+        return make_response(jsonify({"url": new_review_link}), 201)
+    else:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
 
 # Retrieve request for all reviews
-@app.route("/api/v1.0/businesses/<string:id>/reviews", methods=["GET"])
-def fetch_all_reviews(id):
-    if id not in businesses:
+@app.route("/api/v1.0/businesses/<string:b_id>/reviews", methods=["GET"])
+def fetch_all_reviews(b_id):
+    try:
+        business = businesses.find_one({"_id": ObjectId(b_id)}, \
+            {"reviews": 1, "_id": 0})
+    except:
         return make_response(jsonify({"error": "Invalid business ID"}), 404)
     
-    page_num, page_size = 1, 10
-    if request.args.get('pn'):
-        page_num = int(request.args.get('pn'))
-    if request.args.get('ps'):
-        page_size = int(request.args.get('ps'))
-        
-    page_start = (page_size * (page_num - 1))
-    reviews_list = [{k : v} for k,v in businesses[id]["reviews"].items()]
-    data_to_return = reviews_list[page_start : page_start+page_size]
+    if business is None:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
+    
+    data_to_return = []
+    for review in business["reviews"]:
+        review["_id"] = str(review["_id"])
+        data_to_return.append(review)
 
     return make_response( jsonify( data_to_return ), 200 )
 
@@ -129,65 +156,85 @@ def fetch_all_reviews(id):
 @app.route("/api/v1.0/businesses/<string:b_id>/reviews/<string:r_id>", 
     methods=["GET"])
 def fetch_one_review(b_id, r_id):
-    if b_id not in businesses:
-        return make_response(jsonify({"error": "Invalid business ID"}), 404)
-    elif r_id not in businesses[b_id]["reviews"]:
-        return make_response(jsonify({"error": "Invalid review ID"}), 404)
+    try:
+        r_idObject = ObjectId(r_id)
+    except:
+        return make_response(jsonify( \
+            {"error": "Invalid business or review ID"}), 404)
     
-    return make_response(jsonify(businesses[b_id]["reviews"][r_id]), 200)
+    business = businesses.find_one(
+        {"reviews._id": r_idObject},
+        {"_id": 0, "reviews.$": 1}
+    )
 
-# Create review request
-@app.route("/api/v1.0/businesses/<string:b_id>/reviews", methods=["POST"])
-def add_new_review(b_id):
-    if b_id not in businesses:
-        return make_response(jsonify({"error": "Invalid business ID"}), 404)
-    elif "username" not in request.form \
-    or "comment" not in request.form \
-    or "stars" not in  request.form:
-        return make_response(jsonify({"error": "Missing form data"}), 404)
+    if business is None:
+        return make_response(jsonify( \
+            {"error": "Invalid business or review ID"}), 404)
     
-    new_review_id = str(uuid.uuid1())
-    new_review = {
-        "username": request.form["username"],
-        "comment": request.form["comment"],
-        "stars": request.form["stars"]
-    }
-    businesses[b_id]["reviews"][new_review_id] = new_review
-    return make_response(jsonify({new_review_id: new_review}), 201)
+    review = business['reviews'][0]
+    review['_id'] = str(review['_id'])
+    
+    return make_response(jsonify(review), 200)
 
 # Update review request
 @app.route("/api/v1.0/businesses/<string:b_id>/reviews/<string:r_id>", 
     methods=["PUT"])
 def edit_review(b_id, r_id):
-    if b_id not in businesses:
-        return make_response(jsonify({"error": "Invalid business ID"}), 404)
-    elif r_id not in businesses[b_id]["reviews"]:
-        return make_response(jsonify({"error": "Invalid review ID"}), 404)
-
-    is_edited = False
-    for key, value in request.form.items():
-        if key in businesses[b_id]["reviews"][r_id]:
-            is_edited = True
-            businesses[b_id]["reviews"][r_id][key] = value
-    if is_edited:
-        return make_response(jsonify( \
-        {r_id: businesses[b_id]["reviews"][r_id]}), 201)
-    else:
+    if "username" not in request.form \
+    or "comment" not in request.form \
+    or "stars" not in  request.form:
         return make_response(jsonify({"error": "Missing form data"}), 404)
+    
+    try:
+        r_idObject = ObjectId(r_id)
+    except:
+        return make_response(jsonify( \
+            {"error": "Invalid business or review ID"}), 404)
+    
+    edited_review = {
+        "reviews.$.username": request.form["username"],
+        "reviews.$.comment": request.form["comment"],
+        "reviews.$.stars": request.form["stars"]
+    }
+
+    result = businesses.update_one(
+        {"reviews._id": r_idObject},
+        {"$set": edited_review}
+    )
+
+    if result.matched_count == 1:
+        edit_review_link = "http://localhost:5000/api/v1.0/businesses/" \
+            + b_id + "/reviews/" + r_id
+        return make_response(jsonify({"url": edit_review_link}), 200)
+    else:
+        return make_response(jsonify( \
+            {"error": "Invalid business or review ID"}), 404)
 
 # Delete review request
 @app.route("/api/v1.0/businesses/<string:b_id>/reviews/<string:r_id>", 
     methods=["DELETE"])
 def delete_review(b_id, r_id):
-    if b_id not in businesses:
+    try:
+        b_idObject = ObjectId(b_id)
+    except:
         return make_response(jsonify({"error": "Invalid business ID"}), 404)
-    elif r_id not in businesses[b_id]["reviews"]:
-        return make_response(jsonify({"error": "Invalid review ID"}), 404)
     
-    del businesses[b_id]["reviews"][r_id]
-    return make_response(jsonify({}), 200)
-"""
+    try:
+        r_idObject = ObjectId(r_id)
+    except:
+        return make_response(jsonify( \
+            {"error": "Invalid review ID"}), 404)
+    
+    result = businesses.update_one(
+        {"_id": b_idObject, "reviews._id": r_idObject},
+        {"$pull": {"reviews": {"_id": r_idObject}}}
+    )
 
+    if result.matched_count == 1:
+        return make_response(jsonify({}), 204)
+    else:
+        return make_response(jsonify( \
+            {"error": "Invalid business or review ID"}), 404)
 
 if __name__ == "__main__":
     app.run(debug=True)
