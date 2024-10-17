00fddd741756f75da7ab3ce909bd326d31ea4ce3
Callum Young <Young-C45@ulster.ac.uk>
2024-10-17 15:06:13 +0100

    Lab 8.1

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, make_response, jsonify, request
 from pymongo import MongoClient
 from bson import ObjectId
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecret'
 
 client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.bizDB
@@ -235,6 +240,21 @@ def delete_review(b_id, r_id):
     else:
         return make_response(jsonify( \
             {"error": "Invalid business or review ID"}), 404)
+
+@app.route("/api/v1.0/login", methods=['GET'])
+def login():
+    auth = request.authorization
+    if auth and auth.password == "password":
+        token = jwt.encode({
+            "user": auth.username,
+            "exp": datetime.datetime.now(datetime.UTC) +
+                datetime.timedelta(minutes=30)},
+            app.config['SECRET_KEY'],
+            algorithm="HS256"
+        )
+        return make_response(jsonify({"token": token}), 200)
+    return make_response("Could not verify", 401,\
+        {"WWW-Authenticate": 'Basic realm = "Login required"'})
 
 if __name__ == "__main__":
     app.run(debug=True)
