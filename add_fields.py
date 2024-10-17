7ee6acc32cdd626981b0bb4aa58e57b0de8f23a4
Callum Young <Young-C45@ulster.ac.uk>
2024-10-17 10:49:47 +0100

    Lab 7.1

---
+++ add_fields.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import random
+
+client = MongoClient("mongodb://127.0.0.1:27017")
+db = client.bizDB
+businesses = db.biz
+
+for business in businesses.find():
+    businesses.update_one(
+        { "_id" : business['_id'] },
+        { 
+            "$set" : { 
+                "num_employees" : random.randint(1, 100),
+                "profit" : [
+                    { "year" : "2022", "gross" : random.randint(-500000, 500000) },
+                    { "year" : "2023", "gross" : random.randint(-500000, 500000) },
+                    { "year" : "2024", "gross" : random.randint(-500000, 500000) }
+                ]
+            }, 
+            "$unset" : {
+                "dummy": ""
+            }
+        }
+
+    )
