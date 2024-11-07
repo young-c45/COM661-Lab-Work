cc51b2a844f1c98e734dcac48f94abf52f4b2af5
Callum Young <Young-C45@ulster.ac.uk>
2024-11-07 13:12:48 +0000

    Lab 11.2

---
+++ Angular/bizFE/src/app/businesses.components.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'businesses',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './businesses.component.html',
+    styleUrl: './businesses.component.css'
+})
+export class BusinessesComponent {
+    
+}
