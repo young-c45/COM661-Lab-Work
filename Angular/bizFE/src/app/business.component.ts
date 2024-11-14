20c14af0b3c7fe16de6f93c610a5aecc91d4270f
Callum Young <Young-C45@ulster.ac.uk>
2024-11-14 15:55:27 +0000

    Lab 14.2

--- Angular/bizFE/src/app/business.component.ts
+++ Angular/bizFE/src/app/business.component.ts
@@ -2,17 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet, ActivatedRoute } from '@angular/router';
 import { DataService } from './data.service';
 import { CommonModule } from '@angular/common';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
     selector: 'business',
     standalone: true,
-    imports: [RouterOutlet, CommonModule],
+    imports: [RouterOutlet, CommonModule, GoogleMapsModule],
     providers: [DataService],
     templateUrl: './business.component.html',
     styleUrl: './business.component.css'
 })
 export class BusinessComponent {
     business_list: any;
+    business_lat: any;
+    business_lng: any;
+    map_options: google.maps.MapOptions = {};
+    map_locations: any[] = [];
 
     constructor(public dataService: DataService,
         public route: ActivatedRoute) { }
@@ -20,5 +25,19 @@ export class BusinessComponent {
     ngOnInit() {
         this.business_list = this.dataService.getBusiness(
             this.route.snapshot.paramMap.get('id'));
+        this.business_lat = this.business_list[0].location.coordinates[0];
+        this.business_lng = this.business_list[0].location.coordinates[1];
+
+        this.map_locations.push({
+            lat: this.business_lat,
+            lng: this.business_lng
+        })
+
+        this.map_options = {
+            mapId: "DEMO_MAP_ID",
+            center: { lat: this.business_lat, lng: this.business_lng },
+            zoom: 13,
+            disableDefaultUI: true
+        };
     }
 }
