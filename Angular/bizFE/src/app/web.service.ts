6bb59ed8e94ddf7db9d39c4687f70ce6e86f33d0
Callum Young <Young-C45@ulster.ac.uk>
2024-12-12 12:37:09 +0000

    Lab 20.2

--- Angular/bizFE/src/app/web.service.ts
+++ Angular/bizFE/src/app/web.service.ts
@@ -1,12 +1,29 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+/**
+ * The Web Service provides access to the endpoints of the
+ * Biz Directory API.
+ */
 @Injectable()
 export class WebService {
+    /**
+     * The default page size to be returned
+     */
     pageSize: number = 4;
 
+    /**
+     * The constructor for the Web Service
+     * @param http Injecting the HttpClient to the WebService
+     * class
+     JSDoc comments for other components*/
     constructor(private http: HttpClient) {  }
 
+    /**
+     * Fetch a page of businesses from the Biz Directory API
+     * @param page The page number requested
+     * @returns An Observable for the collection of businesses
+     */
     getBusinesses(page: number) {
         return this.http.get<any>(
             'http://localhost:5000/api/v1.0/businesses?pn=' + page + 
